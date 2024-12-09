9c92505ea92a8e783a4b4303e28a6f034a26a51e
ATeals <ateals@icloud.com>
2024-12-10 00:10:24 +0900

    feat: counter + seo

--- src/service/mdx/type.ts
+++ src/service/mdx/type.ts
@@ -1,6 +1,8 @@
 import { type CollectionEntry } from 'astro:content';
 
-export type CollectionData = CollectionEntry<'dev' | 'docs' | 'snippet'> & { href: string };
+export type CollectionData = CollectionEntry<'dev' | 'docs' | 'snippet'> & {
+  data: { image: string; description: string };
+} & { href: string };
 
 export type AllCollectionEntry = CollectionEntry<'dev'> | CollectionEntry<'docs'> | CollectionEntry<'snippet'>;
 
