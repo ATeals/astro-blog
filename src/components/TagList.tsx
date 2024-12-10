6d282c1fd5302c3791749f8b256818c2edbbb48e
ATeals <ateals@icloud.com>
2024-12-10 13:19:57 +0900

    feat: tag, time

---
+++ src/components/TagList.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@/lib/utils';
+
+import { Badge } from './ui/badge';
+
+export const TagList = ({ tags, className }: { tags?: string[]; className?: string }) => {
+  return <ul className={cn(className)}>{tags?.map((tag) => <Badge className="mr-1 text-[0.5rem]">{tag}</Badge>)}</ul>;
+};
