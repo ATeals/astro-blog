57449008e8a280f1e7b81cb6ffd23e231fd5f251
ATeals <ateals@icloud.com>
2024-12-13 15:05:11 +0900

    feat: img caption

--- src/components/MDX/IMG.tsx
+++ src/components/MDX/IMG.tsx
@@ -1,8 +1,10 @@
 export const IMG = ({ src, alt, title }: { src: string; alt: string; title?: string }) => {
+  const caption = alt.includes('caption:') ? alt.split('caption:')[1] : null;
+
   return (
-    <div className="mx-auto py-10">
+    <div className="mx-auto">
       <img src={src} alt={alt} title={title} className="my-0" />
-      {alt && <p className="mt-0 text-center text-sm text-gray-400">{alt}</p>}
+      {caption && <p className="mt-0 text-center text-sm text-gray-400">{caption}</p>}
     </div>
   );
 };
