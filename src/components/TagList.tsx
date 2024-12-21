69ec2f3372bafae58676fb14423051ede58cee3d
ATeals <ateals@icloud.com>
2024-12-21 21:23:04 +0900

    fix: TagList 컴포넌트에서 Badge에 key 속성 추가

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -3,5 +3,13 @@ import { cn } from '@/lib/utils';
 import { Badge } from './ui/badge';
 
 export const TagList = ({ tags, className }: { tags?: string[]; className?: string }) => {
-  return <ul className={cn(className)}>{tags?.map((tag) => <Badge className="mr-1 text-[0.5rem]">{tag}</Badge>)}</ul>;
+  return (
+    <ul className={cn(className)}>
+      {tags?.map((tag) => (
+        <Badge key={tag} className="mr-1 text-[0.5rem]">
+          {tag}
+        </Badge>
+      ))}
+    </ul>
+  );
 };
